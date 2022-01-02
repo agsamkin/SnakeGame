164ffb11a1477d6eefe8bab2f4eca514e5dd120c
ags <a.g.samkin@gmail.com>
2022-01-02 22:01:30 +0300

    ok

--- src/GameField.java
+++ src/GameField.java
@@ -1,7 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
-public class GameField extends JPanel {
+public class GameField extends JPanel implements ActionListener {
 
     private final int SIZE = 320;
     private final int DOT_SIZE = 16;
@@ -30,6 +33,7 @@ public class GameField extends JPanel {
     public GameField() {
         setBackground(Color.BLACK);
         loadImages();
+        initGame();
     }
 
     public void loadImages() {
@@ -39,4 +43,92 @@ public class GameField extends JPanel {
         dot = iid.getImage();
     }
 
+    public void initGame() {
+        dots = 3;
+        for (int i = 0; i < dots; i++) {
+            x[i] = 48 - i*DOT_SIZE;
+            y[i] = 48;
+        }
+
+        timer = new Timer(250, this);
+        timer.start();
+        
+        createApple();
+    }
+
+    private void createApple() {
+        appleX = new Random().nextInt(20)*DOT_SIZE;
+        appleY = new Random().nextInt(20)*DOT_SIZE;
+    }
+
+    // Данный метод будет использоваться таймером
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (inGame) {
+            checkApple();
+            checkCollisions();
+            move();
+        }
+        repaint();
+    }
+
+    private void checkCollisions() {
+
+        for (int i = dots; i > 0; i--) {
+            if (i>4 && x[0] == x[i] && y[0] == y[i]) {
+                inGame = false;
+            }
+        }
+
+        if (x[0] > SIZE) {
+            inGame = false;
+        }
+        if (x[0] < 0) {
+            inGame = false;
+        }
+        if (y[0] > SIZE) {
+            inGame = false;
+        }
+        if (y[0] < 0) {
+            inGame = false;
+        }
+
+    }
+
+    private void checkApple() {
+        if (x[0] == appleX && y[0] == appleY) {
+            dots++;
+            createApple();
+        }
+    }
+
+    private void move() {
+        for (int i = dots; i > 0; i--) {
+            x[i] = x[i-1];
+            y[i] = y[i-1];
+        }
+        if (left) {
+            x[0] -= DOT_SIZE;
+        };
+        if (right) {
+            x[0] += DOT_SIZE;
+        }
+        if (up) {
+            y[0] -= DOT_SIZE;
+        };
+        if (down) {
+            y[0] += DOT_SIZE;
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (inGame) {
+            g.drawImage(apple, appleX, appleY, this);
+            for (int i = 0; i < dots; i++) {
+                g.drawImage(dot, x[i], y[i], this);
+            }
+        }
+    }
 }
